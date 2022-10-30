8cb3324f097ec2a6d6678d7d132414a09faa275c
miyyuk <orion.my.2@icloud.com>
2022-10-30 18:08:18 +0900

    add query and mutation component

--- photo-share-client/src/App.js
+++ photo-share-client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { gql } from 'apollo-boost';
+import Users from './Users';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const ROOT_QUERY = gql`
+  query allUsers {
+    totalUsers
+    allUsers {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`
+
+const App = () => <Users />
 
 export default App;
